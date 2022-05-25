0f53d77ca61258e29cb63630cb50276f6aa2eda6
Friederike L. Bauer <52316159+friederikelbauer@users.noreply.github.com>
2022-05-25 09:44:38 +0200

    adding a HelperFunction page

---
+++ otree-example/survey_example_appfolder/HelperFunctions.py
@@ -0,0 +1,9 @@
+import random
+
+def random_number(x, y):
+    '''
+    method for random integers
+    '''  
+    rng = random.Random()
+    number = rng.randint(x, y)
+    return number
--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -12,6 +12,10 @@ from otree.api import (
 #this is where we would import andy extra functions or packages we need from python
 import random 
 
+#we could also have a python script with custom functions in another file that we can import
+from survey_example_appfolder.HelperFunctions import random_number
+
+
 author = 'your names and team objective go here'
 doc = 'Your app description goes here'
 
@@ -29,6 +33,8 @@ class Subsession(BaseSubsession):
             #here we want to declare the players to different groups (2 in total)
             #we use a python function here from 'random' we imported earlier
             p.group_assignment = random.Random().randint(0, 1)
+            #or:
+            #p.group_assignment = random_number(0,2)
     
 class Group(BaseGroup):
     counter = models.IntegerField(initial = 0)
@@ -39,22 +45,23 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     #this is the most important feature of this file. We can collect all the variables used on the html pages here
     
-#The Variables are structured on the base of pages
-    group_assignment = models.IntegerField() #the variable we declared on top
-
+    #Welcome
     device_type = models.IntegerField()
     operating_system = models.IntegerField()
     screen_height = models.IntegerField(initial=-999)
     screen_width = models.IntegerField(initial=-999)
-    
     entry_question = models.StringField(blank = True) #this is an optional field through blank = True
+    # DemoPage
     age_question = models.IntegerField(max=110, min=1)  #we can also have max and min guidelines
     gender = models.IntegerField(initial=-999, label='Gender Question')  #we can add an initial value or a different label
     hidden_input = models.IntegerField(initial=50, blank=True)
+    #PopoutPage
     popout_question = models.IntegerField(blank=True)
     popout_yes = models.StringField(blank=True)
     popout_no = models.StringField(blank=True)
     time_popout = models.StringField(initial='-999')
+    #EndPage
+    group_assignment = models.IntegerField() #the variable we declared on top
 
 
     #custom error message
